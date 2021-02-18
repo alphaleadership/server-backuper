2cf25b6ea0e6b4112f27aa26069f443eae65efc2
Sly-Little-Fox <69681150+Sly-Little-Fox@users.noreply.github.com>
2021-02-18 10:23:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,59 +1,29 @@
-const Discord = require('discord.js');
-const {
-  createCanvas,
-  loadImage,
-  CanvasRenderingContext2D
-} = require('canvas');
+const https = require('https');
 
-const client = new Discord.Client();
+const data = 'Done!';
 
-client.on('ready', () => {
-  console.log('I am ready!');
-});
-
-client.on('message', async (message) => {
-  if (message.content.startsWith('b!catcake')) {
-    let canvas = createCanvas(200, 270);
-    let ctx = canvas.getContext('2d');
-    ctx.font = `30px Monsterrat`;
-    ctx.fillStyle = '#FAFAFA';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    loadImage('кот.png').then((image) => {
-      ctx.drawImage(image, 50, 20, 70, 70);
-    });
-    loadImage('pancake.png').then((image) => {
-      ctx.drawImage(image, 50, 120, 70, 70);
-    });
-    ctx.fillStyle = '#111111';
-    ctx.fillText('+', 70, 120);
-    ctx.fillText('=', 70, 220);
-    ctx.fillText(message.content.slice(9).trim(), 70 - (message.content.slice(9).trim().length * 4.5), 250);
-    let attachment = new Discord.MessageAttachment(canvas.createPNGStream(), 'image.png')
-    let embed = new Discord.MessageEmbed({
-      "title": "Котоблин",
-      "color": 11088434,
-      "image": {
-        "data": canvas.toBuffer()
-      }
-    }).setTitle('Котоблин').setColor(11088434).attachFiles(attachment).setImage('attachment://image.png');;
-    message.channel.send(embed);
+const options = {
+  hostname: 'api.keyvalue.xyz',
+  port: 443,
+  path: '/8c745558/myKey',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'text/plain',
+    'Content-Length': data.length
   }
-  // if (message.content.startsWith('b!statistics')) {
-  //   let canvas = createCanvas(400, 200);
-  //   let ctx = canvas.getContext('2d');
-  //   ctx.fillStyle = '#e34a4d';
-  //   diagram.createRows([170, 120, 150]);
-  //   diagram.lastx = 0;
-  //   let attachment = new Discord.MessageAttachment(canvas.createPNGStream(), 'image.png')
-  //   let embed = new Discord.MessageEmbed({
-  //     "title": "Котоблин",
-  //     "color": 11088434,
-  //     "image": {
-  //       "data": canvas.toBuffer()
-  //     }
-  //   }).setTitle(`Statistics for ${message.guild.name}`).setColor('#e34a4d').attachFiles(attachment).setImage('attachment://image.png');;
-  //   message.channel.send(embed);
-  // }
-});
+}
 
-client.login(process.env.TOKEN);
+const req = https.request(options, res => {
+  console.log(`statusCode: ${res.statusCode}`)
+
+  res.on('data', d => {
+    process.stdout.write(d)
+  })
+})
+
+req.on('error', error => {
+  console.error(error)
+})
+
+req.write(data)
+req.end();
