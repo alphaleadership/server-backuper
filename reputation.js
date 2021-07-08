a161ae3932051410a71cab1349b72a758a531532
Lynx <slylittlefoxo@gmail.com>
2021-07-08 20:04:50 +0300

    Fix a bug in caching

--- reputation.js
+++ reputation.js
@@ -1,39 +1,51 @@
-'use strict';
+"use strict";
 
-const mem = require('mem');
+const mem = require("mem");
 
 function set(user, guild, reputation, db) {
   return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM reputation WHERE user = ? AND guild = ? LIMIT 1', [user, guild], (err, row) => {
-      if (err) return reject(err);
-      if (row) {
-        db.run('UPDATE reputation SET reputation = ? WHERE user = ? AND guild = ?', [
-          reputation, user, guild
-        ], (err) => {
-          if (err) return reject(err);
-          resolve(reputation);
-        });
-      } else {
-        db.run('INSERT INTO reputation VALUES (?, ?, ?)', [
-          reputation, user, guild
-        ], (err) => {
-          if (err) {
-            return reject(err);
-          } else {
-            resolve(reputation);
-          }
-        });
+    db.get(
+      "SELECT * FROM reputation WHERE user = ? AND guild = ? LIMIT 1",
+      [user, guild],
+      (err, row) => {
+        if (err) return reject(err);
+        if (row) {
+          db.run(
+            "UPDATE reputation SET reputation = ? WHERE user = ? AND guild = ?",
+            [reputation, user, guild],
+            (err) => {
+              if (err) return reject(err);
+              resolve(reputation);
+            }
+          );
+        } else {
+          db.run(
+            "INSERT INTO reputation VALUES (?, ?, ?)",
+            [reputation, user, guild],
+            (err) => {
+              if (err) {
+                return reject(err);
+              } else {
+                resolve(reputation);
+              }
+            }
+          );
+        }
       }
-    });
+    );
   });
 }
 
 function get(user, guild, db) {
   return new Promise((resolve, reject) => {
-    db.get('SELECT * FROM reputation WHERE user = ? AND guild = ? LIMIT 1', [user, guild], (err, row) => {
-      if (err) return reject(err);
-      resolve(row ? row.reputation : 0);
-    });
+    db.get(
+      "SELECT * FROM reputation WHERE user = ? AND guild = ? LIMIT 1",
+      [user, guild],
+      (err, row) => {
+        if (err) return reject(err);
+        resolve(row ? row.reputation : 0);
+      }
+    );
   });
 }
 
@@ -44,16 +56,9 @@ async function adjust(user, guild, confidence, score, actionCount, db) {
 }
 
 module.exports = {
-  setReputation: mem(set, {
-    cacheKey: arguments_ => arguments_.slice(0, 3).join(','),
-    maxAge: 10000
-  }),
+  setReputation: set,
   getReputation: mem(get, {
-    cacheKey: arguments_ => arguments_.slice(0, 2).join(','),
-    maxAge: 10000
+    maxAge: 10000,
   }),
-  adjustReputation: mem(adjust, {
-    cacheKey: arguments_ => arguments_.slice(0, 5).join(','),
-    maxAge: 10000
-  }),
+  adjustReputation: adjust,
 };
